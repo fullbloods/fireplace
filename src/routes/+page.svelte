2a9e0196fe7c5dbfc0f50cb05a00f4b20b303cad
gy1232 <xguoying0822@gmail.com>
2024-11-14 10:07:37 +0900

    세팅

---
+++ src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { goto } from '$app/navigation';
+	let src = '/images/homeImg.png';
+
+	const goTOCreateFireplace = () => {
+		goto('/createFireplace');
+	};
+</script>
+
+<div class="homeContainer">
+	<img {src} alt="눈 배경" class="snowImg" />
+	<div class="title">벽난로 만들기</div>
+	<button class="createBtn" onclick={goTOCreateFireplace}>🔥 나의 벽난로 만들기</button>
+	<div class="snsText">문의 : <a href="#">fireplace_developer</a></div>
+</div>
+
+<style>
+	.homeContainer {
+		width: 100%;
+		height: 100vh;
+		background-color: #fff;
+	}
+
+	.snowImg {
+		margin-bottom: 55px;
+	}
+
+	.title {
+		font-size: 34px;
+		text-align: center;
+		margin-bottom: 55px;
+	}
+
+	.createBtn {
+		width: 220px;
+		height: 82px;
+		background-color: #ff6c6c;
+		border: none;
+		border-radius: 30px;
+		font-size: 16px;
+		color: #fff;
+		justify-content: center;
+		align-items: center;
+		margin: 0 auto;
+		display: flex;
+	}
+
+	.snsText {
+		position: fixed;
+		bottom: 20px;
+		left: 0;
+		right: 0;
+		text-align: center;
+		color: #aaaaaa;
+		font-size: 12px;
+	}
+
+	.snsText > a {
+		color: #aaaaaa;
+		text-decoration: none;
+	}
+</style>
---
+++ src/routes/createFireplace/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+	let name = '';
+	let password = '';
+
+	const goBack = () => {
+		window.history.back();
+	};
+</script>
+
+<div class="createFireplaceContainer">
+	<form>
+		<div class="customInputContainer">
+			<input
+				class="customInput"
+				type="text"
+				bind:value={name}
+				maxlength="8"
+				placeholder="이름(최대 8자)"
+			/>
+			<span class="suffix">의 벽난로</span>
+		</div>
+		<input class="customInput" type="password" bind:value={password} placeholder="비밀번호 입력" />
+		<div class="btnContainer">
+			<button class="customBtn" style="background-color: #FFE51E;">벽난로 만들기 완료</button>
+			<button class="customBtn" onclick={goBack}>닫기</button>
+		</div>
+	</form>
+</div>
+
+<style>
+	.createFireplaceContainer {
+		width: 100%;
+		height: 100vh;
+		background-color: #114433;
+		padding: 100px 50px;
+	}
+
+	.customInputContainer {
+		position: relative;
+		width: 290px;
+	}
+
+	.customInput {
+		width: 290px;
+		height: 40px;
+		padding-right: 80px;
+		padding-left: 10px;
+		border-radius: 10px;
+		border: none;
+		font-size: 18px;
+		margin-bottom: 20px;
+	}
+
+	.suffix {
+		position: absolute;
+		right: 110px;
+		top: 35%;
+		transform: translateY(-50%);
+		font-size: 18px;
+		pointer-events: none;
+	}
+
+	.btnContainer {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.customBtn {
+		width: 290px;
+		height: 40px;
+		border-radius: 10px;
+		border: none;
+		margin-top: 20px;
+		background-color: #fff;
+		font-size: 18px;
+		color: #000;
+	}
+</style>
