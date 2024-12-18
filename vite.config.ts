09f044fbfaa3e01307e21edf9515b7553a7e954f
gy1232 <xguoying0822@gmail.com>
2024-12-18 15:51:48 +0900

    cors 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vitest/config';
-import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from "vitest/config";
+import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
-		include: ['gsap']
+		include: ["gsap"]
 	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ["src/**/*.{test,spec}.{js,ts}"]
+	},
+	server: {
+		cors: {
+			origin: "https://fireplace.fullbloods.site",
+			credentials: true
+		}
 	}
 });
